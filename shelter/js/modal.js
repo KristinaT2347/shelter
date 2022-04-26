f718050b6fe1850c7a80e4a706f5bc10cb14cf8f
Kristina <kristinatabula@icloud.com>
2022-04-26 18:17:42 +0300

    refactor: fix pages generation

--- shelter/js/modal.js
+++ shelter/js/modal.js
@@ -5,21 +5,19 @@ const modalOverlay = document.querySelector(".modal-overlay")
 const openModal = (pet) => {
   modal.classList.toggle("modal-container--active")
   modal.classList.add("fade-in")
-  document.body.classList.add('is-modal-open');
 
   document.getElementById('modal-image').setAttribute('src', pet.img)
   document.getElementById('modal-title').textContent = pet.name
   document.getElementById('modal-breed').textContent = pet.breed
   document.getElementById('modal-description').textContent = pet.description
   document.getElementById('modal-age').textContent = pet.age
-  document.getElementById('modal-inoculations').textContent = pet.inoculations.join(',')
-  document.getElementById('modal-diseases').textContent = pet.diseases.join(',')
-  document.getElementById('modal-parasites').textContent = pet.parasites.join(',')
+  document.getElementById('modal-inoculations').textContent = pet.inoculations.join(', ')
+  document.getElementById('modal-diseases').textContent = pet.diseases.join(', ')
+  document.getElementById('modal-parasites').textContent = pet.parasites.join(', ')
 }
 
 closeButtonModal.addEventListener("click", () => {
   modal.classList.add("fade-out")
-  document.body.classList.remove('is-modal-open')
   setTimeout(() => {
     modal.classList.toggle("modal-container--active")
   }, 200)
@@ -27,7 +25,6 @@ closeButtonModal.addEventListener("click", () => {
 
 modalOverlay.addEventListener("click", () => {
   modal.classList.add("fade-out")
-  document.body.classList.remove('is-modal-open')
   setTimeout(() => {
     modal.classList.toggle("modal-container--active")
   }, 200)
--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,121 +1,127 @@
-import allPets from '../../js/pets.js';
-import { openModal } from '../../js/modal.js';
-import { createCard, chunkArray, getRandomPets } from '../../js/main.js';
+import allPets from "../../js/pets.js"
+import { openModal } from "../../js/modal.js"
+import { createCard, chunkArray, getRandomPets } from "../../js/main.js"
 
-let PAGES = [];
-let PAGE = 0;
+let PAGES = []
+let PAGE = 0
 
 const generatePages = () => {
-  let pets = [];
+  let pets = []
+  const tempPets = getRandomPets(allPets)
 
   for (let i = 0; i < 6; i++) {
-    pets = [...pets, ...getRandomPets(allPets)];
+    pets = [...pets, ...tempPets]
   }
 
   if (window.innerWidth < 768) {
-    return chunkArray(pets, 3);
+    return chunkArray(pets, 3)
   }
 
   if (window.innerWidth < 1280) {
-    return chunkArray(pets, 6);
+    return chunkArray(pets, 6)
   }
 
-  return chunkArray(pets, 8);
-};
+  pets = []
+  for (let i = 0; i < 6; i++) {
+    pets = [...pets, ...getRandomPets(allPets)]
+  }
 
-const firstPageButton = document.getElementById('first-page');
-const lastPageButton = document.getElementById('last-page');
-const prevPageButton = document.getElementById('prev-page');
-const nextPageButton = document.getElementById('next-page');
-const currentPage = document.getElementById('current-page');
-const cardsContainer = document.getElementById('cards-container');
+  return chunkArray(pets, 8)
+}
+
+const firstPageButton = document.getElementById("first-page")
+const lastPageButton = document.getElementById("last-page")
+const prevPageButton = document.getElementById("prev-page")
+const nextPageButton = document.getElementById("next-page")
+const currentPage = document.getElementById("current-page")
+const cardsContainer = document.getElementById("cards-container")
 
 const generatePage = () => {
-  let html = '';
-  PAGES[PAGE].forEach((pet) => (html += createCard(pet)));
+  let html = ""
+  PAGES[PAGE].forEach((pet) => (html += createCard(pet)))
 
-  return html;
-};
+  return html
+}
 
 const onChangePage = () => {
-  cardsContainer.classList.add('fade-out');
+  cardsContainer.classList.add("fade-out")
 
-  currentPage.textContent = PAGE + 1;
+  currentPage.textContent = PAGE + 1
 
   if (PAGE === 0) {
-    firstPageButton.setAttribute('disabled', true);
-    prevPageButton.setAttribute('disabled', true);
+    firstPageButton.setAttribute("disabled", true)
+    prevPageButton.setAttribute("disabled", true)
   } else {
-    firstPageButton.removeAttribute('disabled');
-    prevPageButton.removeAttribute('disabled');
+    firstPageButton.removeAttribute("disabled")
+    prevPageButton.removeAttribute("disabled")
   }
 
   if (PAGE + 1 === PAGES.length) {
-    nextPageButton.setAttribute('disabled', true);
-    lastPageButton.setAttribute('disabled', true);
+    nextPageButton.setAttribute("disabled", true)
+    lastPageButton.setAttribute("disabled", true)
   } else {
-    nextPageButton.removeAttribute('disabled');
-    lastPageButton.removeAttribute('disabled');
+    nextPageButton.removeAttribute("disabled")
+    lastPageButton.removeAttribute("disabled")
   }
-};
+}
 
-PAGES = generatePages();
-cardsContainer.innerHTML = generatePage();
+PAGES = generatePages()
+cardsContainer.innerHTML = generatePage()
 
-const cards = cardsContainer.querySelectorAll('.card');
+const cards = cardsContainer.querySelectorAll(".card")
 
 cards.forEach((card) => {
-  card.addEventListener('click', (e) => {
+  card.addEventListener("click", (e) => {
     const pet = allPets.find(
       (item) => item.name === e.currentTarget.dataset.name
-    );
-    openModal(pet);
-  });
-});
+    )
+    openModal(pet)
+  })
+})
 
-firstPageButton.addEventListener('click', () => {
-  PAGE = 0;
-  onChangePage();
-});
+firstPageButton.addEventListener("click", () => {
+  PAGE = 0
+  onChangePage()
+})
 
-prevPageButton.addEventListener('click', () => {
-  PAGE--;
-  onChangePage();
-});
+prevPageButton.addEventListener("click", () => {
+  PAGE--
+  onChangePage()
+})
 
-nextPageButton.addEventListener('click', () => {
-  PAGE++;
-  onChangePage();
-});
+nextPageButton.addEventListener("click", () => {
+  PAGE++
+  onChangePage()
+})
 
-lastPageButton.addEventListener('click', () => {
-  PAGE = PAGES.length - 1;
-  onChangePage();
-});
+lastPageButton.addEventListener("click", () => {
+  PAGE = PAGES.length - 1
+  onChangePage()
+})
 
-cardsContainer.addEventListener('animationend', (animationEvent) => {
-  if (animationEvent.animationName === 'fade-out') {
-    cardsContainer.innerHTML = generatePage();
-    cardsContainer.classList.remove('fade-out');
-    cardsContainer.classList.add('fade-in');
-    const cards = cardsContainer.querySelectorAll('.card');
+cardsContainer.addEventListener("animationend", (animationEvent) => {
+  if (animationEvent.animationName === "fade-out") {
+    cardsContainer.innerHTML = generatePage()
+    cardsContainer.classList.remove("fade-out")
+    cardsContainer.classList.add("fade-in")
+    const cards = cardsContainer.querySelectorAll(".card")
 
     cards.forEach((card) => {
-      card.addEventListener('click', (e) => {
-        console.log(e.currentTarget.dataset.name);
+      card.addEventListener("click", (e) => {
+        console.log(e.currentTarget.dataset.name)
         const pet = allPets.find(
           (item) => item.name === e.currentTarget.dataset.name
-        );
-        openModal(pet);
-      });
-    });
+        )
+        openModal(pet)
+      })
+    })
   } else {
-    cardsContainer.classList.remove('fade-in');
+    cardsContainer.classList.remove("fade-in")
   }
-});
+})
 
-window.addEventListener('resize', () => {
-  PAGES = generatePages();
-  PAGE = 0;
-  onChangePage();
-});
+window.addEventListener("resize", () => {
+  PAGES = generatePages()
+  PAGE = 0
+  onChangePage()
+})
